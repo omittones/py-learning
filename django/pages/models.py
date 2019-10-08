8a7168035f83d19f6ffd1ff282d35389c2723065
Augustin Juricic <augustin.juricic@gmail.com>
2019-10-08 13:18:30 +0200

    Continue learning.

--- django/pages/models.py
+++ django/pages/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    name = models.CharField(max_length=200, blank=True)
+    text = models.TextField()
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Post named '{self.name}'"
+
+    # def __init__(self, *args, **kwargs):
+    #     super.__init__(self, *args, **kwargs)
--- django/pages/views.py
+++ django/pages/views.py
@@ -1,7 +1,10 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import Post
 
-class HomePageView(TemplateView):
+class HomePageView(ListView):
     template_name = 'home.html'
+    model = Post
+    context_object_name = 'posts' # used in template to reference list of Posts
 
 class AboutPageView(TemplateView):
     template_name = 'about.html'
