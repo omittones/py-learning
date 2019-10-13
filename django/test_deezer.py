24fb86d61d2b11fbabe30bd5347df837cbaec0e4
Augustin Juricic <augustin.juricic@gmail.com>
2019-10-13 22:47:30 +0200

    Get playlists.

--- django/test_deezer.py
+++ django/test_deezer.py
@@ -1,6 +1,7 @@
 import asyncio
 import deezer
 from services.for_deezer import DeezerAuthenticator
+from typing import List
 
 APP_ID = '173261'
 APP_SECRET = '4ba3efb12aaa0cc531e488d698404e9e'
@@ -14,9 +15,13 @@ def main():
     client = deezer.Client(
         app_id='173261', app_secret='4ba3efb12aaa0cc531e488d698404e9e')
     client.access_token = authenticator.access_token
-    u = client.get_user('me')
-
-    print(u)
+    client.follow_next_links = True
+    u: deezer.User = client.get_user('me')
+    for i in u.get_playlists():
+        print(i.title)
+        for t in i.get_tracks():
+            print('    ',
+                  f'{t.artist.name} - {t.title} (from {t.album.title})')
 
 
 if __name__ == '__main__':
